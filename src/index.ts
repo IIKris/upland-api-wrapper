bc69aa745485c685b2a3939172ef8db1d4a4db3b
IIKris <46223258+IIKris@users.noreply.github.com>
2023-03-08 22:29:23 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// https://github.com/snyk-snippets/modern-npm-package
+// https://snyk.io/blog/best-practices-create-modern-npm-package/#prod-ready
+// https://github.com/mongodb/node-mongodb-native/blob/main/.eslintrc.json
+// https://github.com/coloradude/spypoint-api-wrapper/blob/master/spypoint.js
+// https://github.com/prince-ao/my-anime-list#readme
+
+console.log('!Hello world!')
+
+
+
